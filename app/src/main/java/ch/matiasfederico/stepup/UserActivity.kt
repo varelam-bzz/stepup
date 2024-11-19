d983b89ba6bd05869aa2e4284bcf6e07709c56ad
varelam-bzz <varelam@bzz.ch>
2024-11-19 16:34:19 +0100

    Enable edge to edge user screen

--- app/src/main/java/ch/matiasfederico/stepup/UserActivity.kt
+++ app/src/main/java/ch/matiasfederico/stepup/UserActivity.kt
@@ -3,6 +3,7 @@ package ch.matiasfederico.stepup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
@@ -18,6 +19,7 @@ class UserActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             StepupTheme {
