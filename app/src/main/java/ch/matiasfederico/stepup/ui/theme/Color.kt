6867ef19842582ead3529c18fe8b61fe7d8c0e74
perrifederico <perrif@bzz.ch>
2024-11-21 14:13:01 +0100

    Theme files refactored and commented

--- app/src/main/java/ch/matiasfederico/stepup/ui/theme/Color.kt
+++ app/src/main/java/ch/matiasfederico/stepup/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package ch.matiasfederico.stepup.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Define color palette
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
--- app/src/main/java/ch/matiasfederico/stepup/ui/theme/Theme.kt
+++ app/src/main/java/ch/matiasfederico/stepup/ui/theme/Theme.kt
@@ -7,34 +7,30 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+// Define a light color scheme
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 )
 
 @Composable
 fun StepupTheme(
-    dynamicColor: Boolean = true, content: @Composable () -> Unit
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
+        // Use dynamic color scheme for API 31+
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             dynamicLightColorScheme(context)
         }
-
         else -> LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme, typography = Typography, content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
